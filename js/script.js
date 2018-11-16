9f1f1a669cdb3fc99a9817e8d1eba4fec2cfcd2d
Codeer12 <39409827+Codeer12@users.noreply.github.com>
2018-11-15 22:54:52 -0500

    Batch update

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDP24URuLBe5z8zKnVofQNBL2D8KJuT130",
+    authDomain: "max-neighborhood-grocery.firebaseapp.com",
+    databaseURL: "https://max-neighborhood-grocery.firebaseio.com",
+    projectId: "max-neighborhood-grocery",
+    storageBucket: "max-neighborhood-grocery.appspot.com",
+    messagingSenderId: "773266773070"
+  };
+  firebase.initializeApp(config);
+
+
+  var config = {
+    // ...
+  };
+  firebase.initializeApp(config);
