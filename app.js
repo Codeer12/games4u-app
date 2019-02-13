9994b128a0e98472ffcd1fbc665328a2e170f884
Codeer12 <39409827+Codeer12@users.noreply.github.com>
2019-02-13 09:45:34 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,92 @@
-<script>
-// Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
-// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")
-// Access the device camera and stream to cameraView
-function cameraStart() {
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(function(stream) {
-        track = stream.getTracks()[0];
-        cameraView.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.error("Oops. Something is broken.", error);
-    });
-}
-// Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
-    cameraSensor.width = cameraView.videoWidth;
-    cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
-    cameraOutput.src = cameraSensor.toDataURL("image/webp");
-    cameraOutput.classList.add("taken");
-};
-// Start the video stream when the window loads
-window.addEventListener("load", cameraStart, false);
-</script>
+document.write( '<body class=\"body-initial all-style enable-mobile silex-runtime silex-published\" data-silex-type=\"container\">\n' );
+document.write( '    <div class=\"silex-pages\">\n' );
+document.write( '        <div data-silex-type=\"image\" class=\"editable-style silex-id-hamburger-menu image-element menu-button prevent-draggable hide-on-desktop\">\n' );
+document.write( '            <img alt=\"open mobile menu\" src=\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAoCAYAAAC8cqlMAAAB5UlEQVR42u2ZPWvCUBSGM+UfFO3e3dLu/QN+oKj4EUXRQREEJYgoiCAu6qCogyDo7iJ21UUaKqEgCC4iiEMGB3UVgsnpTWj/wfVK2nvhGbKEPJyT996TMMzPGg6H5mKx+GgkyuWyeTQasb8OTDwef4tEIp8+n+/LYIjJZJJHQiwzm81M6OLDarWqNpsNjEYgEJCy2ewT0+/3TagighElNFAnQT6ft1ARKkJFqMgfFvF4PFpuEwFtdrcRqVarIIoirFYrIiyXSxiPx+B2u/GJBINBuF6vQHopigK1Wg2fCMdxcLlciIuoqgqNRgOfiN1uB3Qwg91uB8fjkQiHwwEmkwne1qLxS0VuKKJle71eh06nQ4Rmswk8z+MVcTqdsNlsQJZlotF7Pp8hk8ngE4lGo/qN7xG/vV4Pb0Xm8zlxkdPpBGgMx/+yJxIJKBQKRMjlcuBwOGhq/Q+RcDgMsViMCFrAuFwu/CKDwQC22y1IkkSE/X4PgiDo+xc2Ea0S9zjGa/Hb7Xbxifj9fj0K7yFSqVTwtlY6nYbpdKpPiSRYLBbQarX0PYymFhWhIlSEilCRW6GdyUqlkoVZr9dsKpXiOY6TtW+7BkNBz/7ebrcf9L+6Xq+XRYPMMyrTq5EIhUIvaAjTJb4BBNQ2yhnth0wAAAAASUVORK5CYII=\" class=\"silex-element-content\">\n' );
+document.write( '        </div>\n' );
+document.write( '\n' );
+document.write( '        <a id=\"page-page-1\" data-silex-type=\"page\" class=\"page-element page-link-active\" href=\"index.html\">Page 1</a><a id=\"page-moto-x3m\" data-silex-type=\"page\" class=\"page-element\" href=\"moto-x3m.html\">Moto X3M</a><a id=\"page-strike-force-heroes\" data-silex-type=\"page\" class=\"page-element\" href=\"strike-force-heroes.html\">Strike Force Heroes</a><a id=\"page-gun-mayhem\" data-silex-type=\"page\" class=\"page-element\" href=\"gun-mayhem.html\">Gun Mayhem</a><a id=\"page-rogue-soul-2\" data-silex-type=\"page\" class=\"page-element\" href=\"rogue-soul-2.html\">Rogue Soul 2</a><a id=\"page-alex-stream\" data-silex-type=\"page\" class=\"page-element\" href=\"alex-stream.html\">alex stream</a><a id=\"page-dbz-devolution\" data-silex-type=\"page\" class=\"page-element\" href=\"dbz-devolution.html\">DBZ Devolution</a><a id=\"page-chat-\" data-silex-type=\"page\" class=\"page-element\" href=\"chat-.html\">Chat </a><a id=\"page-prodigy\" data-silex-type=\"page\" class=\"page-element\" href=\"prodigy.html\">Prodigy</a><a id=\"page-crazy-plant-shop\" data-silex-type=\"page\" class=\"page-element\" href=\"crazy-plant-shop.html\">Crazy Plant Shop</a><a id=\"page-super-smash-flash-2\" data-silex-type=\"page\" class=\"page-element\" href=\"super-smash-flash-2.html\">Super Smash Flash 2</a><a id=\"page-news\" data-silex-type=\"page\" class=\"page-element\" href=\"news.html\">News</a><a id=\"page-mngs\" data-silex-type=\"page\" class=\"page-element\" href=\"mngs.html\">MNGS</a><a id=\"page-mngs-order-to-pickup\" data-silex-type=\"page\" class=\"page-element\" href=\"mngs-order-to-pickup.html\">MNGS Order to Pickup</a><a id=\"page-about\" data-silex-type=\"page\" class=\"page-element\" href=\"about.html\">About</a><a id=\"page-copyright-application\" data-silex-type=\"page\" class=\"page-element\" href=\"copyright-application.html\">Copyright Application</a><a id=\"page-run-3-\" data-silex-type=\"page\" class=\"page-element\" href=\"run-3-.html\">Run 3 </a><a id=\"page-cat-ninja\" href=\"cat-ninja.html\" data-silex-type=\"page\" class=\"page-element\">Cat Ninja</a></div>\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '    \n' );
+document.write( '    \n' );
+document.write( '    \n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '\n' );
+document.write( '<div data-silex-type=\"container\" class=\"prevent-draggable container-element editable-style silex-id-1478366444112-1 section-element\" style=\"\">\n' );
+document.write( '        <div data-silex-type=\"container\" class=\"editable-style silex-element-content silex-id-1478366444112-0 silex-container-content container-element prevent-draggable website-width\"><div data-silex-type=\"html\" class=\"editable-style silex-id-1545614796221-2 html-element\" style=\"\"><div class=\"silex-element-content\">\n' );
+document.write( '<div id=\"quickstart-account-details\"></div>\n' );
+document.write( '<div id=\"quickstart-sign-in\"></div>\n' );
+document.write( '<div id=\"quickstart-sign-in-status\"></div></div></div></div>\n' );
+document.write( '    </div><div data-silex-type=\"container\" class=\"prevent-draggable container-element editable-style silex-id-1474394621033-3 section-element\" style=\"\">\n' );
+document.write( '        <div data-silex-type=\"container\" class=\"editable-style silex-element-content silex-id-1474394621032-2 silex-container-content container-element prevent-draggable website-width\"><div data-silex-type=\"html\" class=\"editable-style silex-id-1545403761432-2 html-element page-page-1 paged-element page-link-active\" style=\"\"><div class=\"silex-element-content\">Welcome to Cerph\'s website! To play games, use the page bar on the side to navigate games!\n' );
+document.write( '<h2>Introducing Websites!</h2>\n' );
+document.write( '\n' );
+document.write( 'To get a website of your own, ask J Camille himself for a website designed and maintained by him.\n' );
+document.write( 'The first website availble is for Cerph at <a href=\"https://codeer12.github.io/cerphmoneus\">https://codeer12.github.io/cerphmoneus</a></div></div></div>\n' );
+document.write( '    </div><div data-silex-type=\"html\" class=\"editable-style silex-id-1542730020762-1 html-element\" style=\"\" title=\"Games4U \"><div class=\"silex-element-content\">Pages\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/moto-x3m\">Moto X3M</a>\n' );
+document.write( '</button>\n' );
+document.write( '\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/strike-force-heroes\">Strike Force Heroes</a>\n' );
+document.write( '</button>\n' );
+document.write( '\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/gun-mayhem\">Gun Mayhem</a>\n' );
+document.write( '</button>\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/rogue-soul-2\">Rogue Soul 2</a>\n' );
+document.write( '</button>\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/\">Home</a>\n' );
+document.write( '</button>\n' );
+document.write( '\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/alex-mixer-stream\">Alex Mixer Stream</a>\n' );
+document.write( '</button>\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/dbz-devolution\">DBZ Devolution</a>\n' );
+document.write( '</button>\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/chat\">Chat</a>\n' );
+document.write( '</button>\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/prodigy\">Prodigy</a>\n' );
+document.write( '</button>\n' );
+document.write( '<button>\n' );
+document.write( '<a href=\"/super-smash-flash-2\">Super Smash Flash 2 (finally!)</a>\n' );
+document.write( '</button>\n' );
+document.write( '\n' );
+document.write( '<button>\n' );
+document.write( ' <a href=\"/cat-ninja\">Cat Ninja</a>\n' );
+document.write( '</button>\n' );
+document.write( '<button>\n' );
+document.write( '    \n' );
+document.write( '    <a href=\"run-3-.html\">Run 3</a>\n' );
+document.write( '</button>\n' );
+document.write( '<button>\n' );
+document.write( '    <a href=\"/news\">News</a>\n' );
+document.write( '</button></div></div>' );
+
