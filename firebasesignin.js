5d632bd1154ce7d1835833dc5b282408a5947111
Codeer12 <39409827+Codeer12@users.noreply.github.com>
2019-02-14 17:31:24 -0500

    Update firebasesignin.js

--- firebasesignin.js
+++ firebasesignin.js
@@ -1,6 +1,18 @@
   /**
      * Function called when clicking the Login/Logout button.
      */
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA8r2vrQaL3f4Jz1Et09RPvJts2xR_sBOg",
+    authDomain: "login-to-games4u.firebaseapp.com",
+    databaseURL: "https://login-to-games4u.firebaseio.com",
+    projectId: "login-to-games4u",
+    storageBucket: "login-to-games4u.appspot.com",
+    messagingSenderId: "152743638512"
+  };
+  firebase.initializeApp(config);
+
   
       // [END getidptoken]
       // Listening for auth state changes.
