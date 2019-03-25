81c321c7e68b66088830d275011f878868f8df5e
Codeer12 <39409827+Codeer12@users.noreply.github.com>
2019-03-25 19:05:47 -0400

    Create  theimpossiblequiz.js

---
+++ theimpossiblequiz.js
@@ -0,0 +1,6 @@
+document.write( '<div class=\"container\" id=\"game\"><div class=\"displaygame infogame\"><script type=\"text/javascript\">var gametype = \'SWF\';\n' );
+document.write( ' var gamewidth = \'0\';\n' );
+document.write( ' var gameheight = \'0\';\n' );
+document.write( ' var setwidth = \'800\';\n' );
+document.write( ' var setheight = \'600\';</script><div id=\"topads\"></div><div id=\"maingamecontainer\"><div style=\"width: 800px;margin:auto\"><div class=\"displaygamecontainer\" id=\"displaygamecontainer\" style=\"width: 800px;height: 600px;\"><object type=\"application/x-shockwave-flash\" data=\"//theimpossible-quiz.com//data/file/swf/theimpossiblequiz.swf\" width=\"100%\" height=\"100%\" id=\"flash_game\" style=\"visibility: visible;\"><param name=\"allowScriptAccess\" value=\"always\"><param name=\"allownetworking\" value=\"internal\"><param name=\"wmode\" value=\"direct\"><param name=\"bgcolor\" value=\"000000\"></object>\n' );
+document.write( ' </div></div></div></div></div>' );
