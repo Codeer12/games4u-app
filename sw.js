bc616d242fd42d703b04c9b2b16c183cc43a7bc4
Codeer12 <39409827+Codeer12@users.noreply.github.com>
2019-07-05 20:10:58 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,13 +5,14 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('airhorner').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/2019/06',
-       '/2019/05',
-       '/2019/06/rolling-green-in-florida-highly.html',
-       '/2019/06/removal-and-explantion.html',
-       '/2019/05/jossaya-and-his-adventures-preview-post.html',
-       '/2019/05/fk-alex-pt-3.html'
+       '/index.html',
+       'https://blog.se-gaming.tk',
+       'https://blog.se-gaming.tk/2019/05',
+       'https://blog.se-gaming.tk/2019/06/rolling-green-in-florida-highly.html',
+       'https://blog.se-gaming.tk/2019/06/removal-and-explantion.html',
+       'https://blog.se-gaming.tk/2019/05/jossaya-and-his-adventures-preview-post.html',
+       'https://blog.se-gaming.tk/2019/05/fk-alex-pt-3.html',
+       'https://blog.se-gaming.tk/2019/06'
      ]);
    })
  );
